4aa893f11b7378f3043caa8088016e515bfc4271
Raul Barrios <barrau89@gmail.com>
2022-11-25 22:07:59 -0600

    can add and delete memos to the db.json and they are sync with front-end

--- src/app/services/ui.service.ts
+++ src/app/services/ui.service.ts
@@ -12,26 +12,38 @@ export class UiService {
     // new Memo(1,'message 2', null, 'sender 2', 'subject 2'),
   ]
   private memosSubject: Subject<Memo[]> = new Subject()
+  private http: HttpClient
 
-  constructor(http: HttpClient) { 
-    http
-    .get<Memo []>('http://localhost:3000/memos')
-    .subscribe(memos => {
-      console.log(memos)
-      this.memos = memos
-      this.memosSubject.next(this.memos)
+  constructor(http: HttpClient) {
+    this.http = http  
+    this.updateMemos()
+  }
+
+  updateMemos (): void {
+    this.http
+      .get<Memo []>('http://localhost:3000/memos')
+      .subscribe(memos => {
+        console.log(memos)
+        this.memos = memos
+        this.memosSubject.next(this.memos)
     })
   }
 
 
   deleteMemoById (id:number) :void {
     console.log('deleteMemoById '+ id)
-    const index = this.memos.findIndex((memo) => {return memo.id === id})
-    this.memos.splice(index,1)
+    // const index = this.memos.findIndex((memo) => {return memo.id === id})
+    // this.memos.splice(index,1)
+    this.http
+      .delete(`http://localhost:3000/memos/${id}`)  //the `` allows to introduce the value of a variable 
+      .subscribe(() => this.updateMemos())         // inside of a string and consider the whole as a string
   }
 
   addMemo (memo: Memo):void {
-    this.memos.push (memo)
+    this.http
+      .post('http://localhost:3000/memos', memo)
+      .subscribe (() => {this.updateMemos()})
+    //this.memos.push (memo)
   }
 
   whenMemosUpdated(): Observable<Memo[]> {
